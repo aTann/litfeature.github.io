03eba89a03c4cedc79260dd6e2bc9f5e19a22e29
LitFeature <glanxiaoyu@gmail.com>
2017-06-22 10:44:42 +0800

    updated demo and post

--- demo/Ajax/ex-book/practice-13.js
+++ demo/Ajax/ex-book/practice-13.js
@@ -17,69 +17,60 @@ JSONP数据源服务（http://api.flickr.com/services/feeds/photos_public.gne）
 
 ----------------------------------------------------------------------------------------------------------*/
 
-$(function () {
-	
+$(function() {
+
 	var $ajaxForm = $('#ajax-form'),
 		$response = $('#response'),
 		noresults = 'There were no reach result.',
 		failed = 'Sorry, but the request could not' +
-				' reach its destination. Try again later.';
+		' reach its destination. Try again later.';
 
 	// 构建HTML
-	var buildPicture = function (items) {
-		
-			// console.log(itemVal);
-		
-		// var $media_link;
-		// $.each(items.media, function (argument) {
-		// 	// body...
-		// })
+	var buildPicture = function(items) {
+
+		// console.log(itemVal);
 
 		var html = '';
 		html += '<li>';
 		html += '<h3>' + items.title + '</h3>';
 		var pattern = /\"(.*)\"/;
 		var author = items.author.match(pattern)[1];
-		// var author = 'nobody@flickr.com ("纱泛读")'.match(pattern)[1];
+		// var author = 'nobody@flickr.com ("纱泛读")'.match(pattern)[1];		// 测试非英文在Regex.match()得到Unicode吗
 		// console.log(author);
-		
-        html += 'author: <a href="https://www.flickr.com/photos/'+ items.author_id +'/">' + author + '</a>';
-        html += '<div class="picture">';
-        // html += '<a href="' + items.link + '"><img src="' + items.media.m + '"></a>';
-        html += '<p>' + items.description + '</p>';
-        html += '<strong>tags:  </strong>';
-        $.each(items.tags.split(' '), function (tagIndex, tag) {
-        	html += '<span>  ' + tag + ',   </span>';
-        });
-        
-        html += '</div>';
-        html += '</li>';
-        // console.log(items);
-		
+
+		html += 'author: <a href="https://www.flickr.com/photos/' + items.author_id + '/">' + author + '</a>';
+		html += '<div class="picture">';
+		// html += '<a href="' + items.link + '"><img src="' + items.media.m + '"></a>';    // 因为得到的数据中自带描述和显示图片，以及相应的页面跳转
+		html += '<p>' + items.description + '</p>';
+		html += '<strong>tags:  </strong>';
+		$.each(items.tags.split(' '), function(tagIndex, tag) {
+			html += '<span>  ' + tag + ',   </span>';
+		});
+		html += '</div>';
+		html += '</li>';
+
 		return html;
 	};
 
-	// response
-	
-	var response = function (data) {
-		var output = '';
-		
-		console.log(data.length);
+	// response，处理成功返回的数据
 
+	var response = function(data) {
+		var output = '';
+		console.log(data)
 		if (data.items && data.items.length) {
 			output += '<ol>';
-			$.each(data.items, function (itemsIndex, items) {
+			$.each(data.items, function(itemsIndex, items) {
 				output += buildPicture(items);
 			});
 			output += '</ol>';
 		} else {
 			output += noresults;
 		};
-				
+
 		$response.html(output);
 	};
 
-	$ajaxForm.on('submit', function (event) {
+	$ajaxForm.on('submit', function(event) {
 		event.preventDefault();
 
 		var tag_search = $('#tags').val();
@@ -88,25 +79,54 @@ $(function () {
 			return;
 		}
 
+		/*
 		$.ajax({
-			url: 'http://api.flickr.com/services/feeds/photos_public.gne',
-			dataType: 'jsonp',
-			jsonp: 'jsoncallback',
-			data: {
-				format: 'json', 	// json¸格式读取标明
-				tags: $('#tags').val()	// tags获取
-				
-			},
-			// 成功获取回应信息
-			// success: response
-		}).done(response)
-		  .fail(function () {
-		  	$response.html(failed);
-		  });
+				url: 'http://api.flickr.com/services/feeds/photos_public.gne',
+				dataType: 'jsonp',
+				jsonp: 'jsoncallback',
+				jsonpCallback: 'flickrCallback',	// 为返回的json数据项命名，如果没有的话，jQuery就会随机命名(eg. jQuery3210376468554153065_1498098544585)
+				data: {
+					format: 'json', // json¸格式读取标明
+					tags: tag_search // tags获取
+
+				},
+				// 成功获取回应信息
+				// success: response
+			}).done(response) // 成功返回，response处理回应的数据，包括回应中无结果
+			.fail(function() { // 错误处理
+				$response.html(failed);
+			});
+		*/
+		
+		var api = {};
+		if(!api[tag_search]){
+			api[tag_search] = $.ajax({
+				url: 'http://api.flickr.com/services/feeds/photos_public.gne',
+				dataType: 'jsonp',
+				jsonp: 'jsoncallback',
+				jsonpCallback: 'flickrCallback',
+				data: {
+					format: 'json', // json¸格式读取标明
+					tags: tag_search, // tags获取
+					timeout: 15000
+				}
+				// 成功获取回应信息
+				// success: response
+			});
+		}
+		api[tag_search].done(response)	// 返回处理
+					   .fail(function() { // 错误处理
+							$response.html(failed);
+						});
+
+
 
 	});
-	
+
 });
+
 
 // ajax灵感来源
 // https://teamtreehouse.com/community/how-do-i-get-flickr-to-respond-json-that-i-can-use
+// 
+
