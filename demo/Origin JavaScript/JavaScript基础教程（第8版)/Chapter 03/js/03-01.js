2aa58c0f1db3345a52ad3df8efb43ce97d79c7a8
LitFeature <glanxiaoyu@gmail.com>
2017-07-15 23:13:11 +0800

    updated demo

---
+++ demo/Origin JavaScript/JavaScript基础教程（第8版)/Chapter 03/js/03-01.js
@@ -0,0 +1,37 @@
+// 过时的探测方式
+// 
+// 1、检查浏览器支持哪些对象
+// 2、替代方式是进行浏览器器探测（browser detection）
+// 
+// 代码编写之后出现新浏览器
+// 浏览器故意报告错误的信息
+// 
+// 
+// 
+// 处理数组：
+// 
+// 声明
+var arr = new Array('Toyota', 'Honda', 'Nissan');  // 初始化
+var arr = new Array(params...?: any);	// 空数组
+var arr = new Array(76); // 76长度
+var arr = []; // 空数组
+
+// 访问数组内容：变量名和数据成员的索引号(index number)
+// 
+// 
+// do/while循环：do 代码块至少运行一次
+// do/while 循环的一种常见用途是，从用户输入的数据中去掉空格或无效的字符。但是要记住，
+// 无论while 检查计算出true 还是false，do 代码块至少会执行一次。
+// 
+// 
+// 组合使用JavaScript和CSS
+// 
+// 位：与(&)、或(|) 位算术（bitwise arithmetic）
+// 
+// 
+// 检查状态：
+// 
+// 
+// 处理字符串数组：
+// 
+// 
