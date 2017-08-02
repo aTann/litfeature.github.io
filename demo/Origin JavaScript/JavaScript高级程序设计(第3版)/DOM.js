6c8067c23a397a6645be41949b45533ea9534ce1
LitFeature <glanxiaoyu@gmail.com>
2017-08-02 18:42:26 +0800

    updated demo

---
+++ demo/Origin JavaScript/JavaScript高级程序设计(第3版)/DOM.js
@@ -0,0 +1,18 @@
+
+
+// 不同节点层次
+// 		Document
+// 		Element
+// 		Text
+// 		Comment
+// 		CDATASection
+// 		DocumentType
+// 		DocumentFragment
+// 		Attr
+
+
+// DOM操作技术
+// 		动态脚本
+// 		动态样式
+// 		操作表格
+// 		使用NodeList
